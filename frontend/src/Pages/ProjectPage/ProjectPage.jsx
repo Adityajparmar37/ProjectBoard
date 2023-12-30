4465764667bc95913391981e9649e95765909f5e
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2023-12-30 12:31:57 +0530

    Project sub page UI Done

---
+++ frontend/src/Pages/ProjectPage/ProjectPage.jsx
@@ -0,0 +1,25 @@
+import MiniCard from "../../Components/MiniCard/MiniCard";
+import { MdOutlineCreateNewFolder } from "react-icons/md";
+import { GrDocumentUpdate } from "react-icons/gr";
+
+export default function ProjectPage() {
+    return (
+            <div className="grid grid-cols-3 gap-5 pt-20 h-screen">
+                <MiniCard
+                    to="/createProject"
+                    bgColor="bg-orange-600"
+                    icon={<MdOutlineCreateNewFolder className="text-[2.5rem] text-orange-600" />}
+                    title="Create Project"
+                    content={["Create a fresh new Project"]}
+                />
+
+                <MiniCard
+                    to="/createProject"
+                    bgColor="bg-orange-600"
+                    icon={<GrDocumentUpdate className="text-[2.5rem] text-orange-600" />}
+                    title="Project Dashboard"
+                    content={["Manage Ongoing Project","Update existing Project"]}
+                />
+            </div>
+    )
+}
