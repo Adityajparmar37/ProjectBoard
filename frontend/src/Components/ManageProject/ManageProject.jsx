9e43f12bf3c5d261f02aa59ad460d2994f305fd4
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2023-12-31 19:31:37 +0530

    minor changes

--- frontend/src/Components/ManageProject/ManageProject.jsx
+++ frontend/src/Components/ManageProject/ManageProject.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useLoading } from '../../Hooks/useLoading';
 import { getAllProject } from '../../Services/projectServices';
 import Filters from '../Filters/Filters';
@@ -30,7 +31,7 @@ export default function ManageProject() {
             <div className="pt-20 bg-gray-100 flex-1 overflow-x-hidden">
                 <div className='grid grid-cols-2 flex-1 overflow-x-hidden'>
                     {studentProjects && studentProjects.length > 0 ? (
-                        studentProjects.map((project,index) => (
+                        studentProjects.map((project, index) => (
                             <ProjectCard
                                 key={index}
                                 Id={project._id}
@@ -41,14 +42,16 @@ export default function ManageProject() {
                         ))
                     ) : (
                         <div className='flex items-center justify-center w-screen h-screen'>
-                            <h1 className=' bg-gray-600 text-white text-lg rounded-md p-2'>No Project Found</h1>
+                            <Link to="/project">
+                                <h1 className=' bg-gray-600 text-white text-lg rounded-md p-2'>No Project Found! Click to go back</h1>
+                            </Link>
                         </div>
                     )}
                 </div>
             </div>
 
             {studentProjects && studentProjects.length > 0 ? (<div className='w-1/4 bg-white shadow-lg  right-0 top-0 h-screen'>
-                <Filters setStudentProjects={setStudentProjects} pageSize={6}/>
+                <Filters setStudentProjects={setStudentProjects} pageSize={6} />
             </div>) : (<></>)}
 
         </div>
