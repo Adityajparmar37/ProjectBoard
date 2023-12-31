dd0ffa613ef54f7ac558b59f0c5732f5725fb272
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2023-12-31 19:36:16 +0530

    minor change in loading

--- frontend/src/Components/Loading/Loading.jsx
+++ frontend/src/Components/Loading/Loading.jsx
@@ -8,7 +8,7 @@ export default function Loading() {
         <div className="fixed w-full h-full bg-WhiteLoading-rgba z-50 top-0 left-0">
             <div className="flex flex-col justify-center items-center w-full h-[80%]">
                 <img src="/loading.svg" alt="Loading!" />
-                <h1 className='text-orange-500'>Loading...</h1>
+                <h1 className='text-orange-500 text-[1.5rem]'>Loading...</h1>
             </div>
         </div>
     );
