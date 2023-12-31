58de85aa99edc20c021ecd4e6076b3dd332e1704
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2023-12-31 17:38:42 +0530

    minor change in header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
             <nav className="bg-white dark:bg-gray-800 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
                 <div className="max-w-screen-xl flex flex-wrap items-end justify-between mx-auto p-4">
                     <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Project Board</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Project Board 📜</span>
                     </Link>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         {student ? (
