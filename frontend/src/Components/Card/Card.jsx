397d72c38fc7a5c75651e9506cae3e5d40b11ef2
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-01-26 09:44:39 +0530

    minor ui change

--- frontend/src/Components/Card/Card.jsx
+++ frontend/src/Components/Card/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Card = ({ to, bgColor, icon, title, content }) => {
     return (
         <Link to={to}>
-            <div className={`w-96 h-56 bg-white rounded-lg m-10 shadow-lg hover:shadow-2xl`}>
+            <div className={`w-96 h-56 bg-white rounded-lg m-10 shadow-lg hover:shadow-2xl duration-500`}>
                 <div className={`w-95 h-12 ${bgColor} rounded-t-lg flex items-center px-8`}>
                     <div className="mt-10 bg-white p-2 rounded-lg">{icon}</div>
                 </div>
