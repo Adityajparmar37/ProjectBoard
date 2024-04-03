5e3b6c18333250de32cd1b752824b91eedb843d7
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-03 11:41:32 +0530

    optimize react with lazy loading 😃

--- frontend/src/Components/Routes/AppRoute/AppRoute.jsx
+++ frontend/src/Components/Routes/AppRoute/AppRoute.jsx
@@ -1,107 +1,192 @@
+import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import DefaultPage from "../../../Pages/DefaultPage/DefaultPage";
-import HomePage from "../../../Pages/HomePage/HomePage";
-import Login from "../../../Pages/Login/Login";
-import SignUp from "../../../Pages/Signup/SignUp";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import AuthRoute from "../AuthRoute/AuthRoute";
-import ProjectPage from "../../../Pages/ProjectPage/ProjectPage";
-import CreateProject from "../../CreateProject/CreateProject";
-import ManageProject from "../../ManageProject/ManageProject";
-import ProjectView from "../../../Pages/ProjectView/ProjectView";
-import ChatPage from "../../../Pages/ChatPage/ChatPage";
-import SearchMember from "../../SearchMember/SearchMember";
-import TaskPage from "../../../Pages/TaskPage/TaskPage";
-import CreateTask from "../../CreateTask/CreateTask";
-import ManageTask from "../../ManageTask/ManageTask";
-import TaskView from "../../../Pages/TaskView/TaskView";
-import Calendar from "../../Calendar/Calendar";
-import FilePage from "../../../Pages/FilePage/FilePage";
-import UploadFile from "../../UploadFile/UploadFile";
-import ManageFile from "../../ManageFile/ManageFile";
-import Chatting from "../../Chatting/Chatting";
-import ProfilePage from "../../../Pages/ProfilePage/ProfilePage";
-import ForgotPassword from "../../../Pages/ForgotPassword/ForgotPassword";
-import ResetPassword from "../../../Pages/ResetPassword/ResetPassword";
+
+// Lazy loaded components
+const DefaultPage = lazy(() =>import("../../../Pages/DefaultPage/DefaultPage"));
+const HomePage = lazy(() => import("../../../Pages/HomePage/HomePage"));
+const Login = lazy(() => import("../../../Pages/Login/Login"));
+const SignUp = lazy(() => import("../../../Pages/Signup/SignUp"));
+const ProjectPage = lazy(() =>import("../../../Pages/ProjectPage/ProjectPage"));
+const CreateProject = lazy(() =>import("../../CreateProject/CreateProject"));
+const ManageProject = lazy(() =>import("../../ManageProject/ManageProject"));
+const ProjectView = lazy(() =>import("../../../Pages/ProjectView/ProjectView"));
+const ChatPage = lazy(() => import("../../../Pages/ChatPage/ChatPage"));
+const SearchMember = lazy(() => import("../../SearchMember/SearchMember"));
+const TaskPage = lazy(() => import("../../../Pages/TaskPage/TaskPage"));
+const CreateTask = lazy(() => import("../../CreateTask/CreateTask"));
+const ManageTask = lazy(() => import("../../ManageTask/ManageTask"));
+const TaskView = lazy(() => import("../../../Pages/TaskView/TaskView"));
+const Calendar = lazy(() => import("../../Calendar/Calendar"));
+const FilePage = lazy(() => import("../../../Pages/FilePage/FilePage"));
+const UploadFile = lazy(() => import("../../UploadFile/UploadFile"));
+const ManageFile = lazy(() => import("../../ManageFile/ManageFile"));
+const Chatting = lazy(() => import("../../Chatting/Chatting"));
+const ProfilePage = lazy(() =>import("../../../Pages/ProfilePage/ProfilePage"));
+const ForgotPassword = lazy(() =>import("../../../Pages/ForgotPassword/ForgotPassword"));
+const ResetPassword = lazy(() =>import("../../../Pages/ResetPassword/ResetPassword"));
 
 export default function AppRoute() {
-    return (
-        <>
-            <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/" element={<DefaultPage />} />
-                <Route path="/forgotPassword" element={<ForgotPassword />} />
-                <Route path="/api/student/newPassword/:id/:token" element={<ResetPassword />} />
+  return (
+    <>
+      <Toaster
+        position="top-center"
+        toastOptions={{ duration: 2000 }}
+      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/login"element={<Login />}/>
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/" element={<DefaultPage />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/api/student/newPassword/:id/:token" element={<ResetPassword />}/>
 
-                {/* main pages */}
-                <Route path="/home" element={<AuthRoute>
-                    <HomePage />
-                </AuthRoute>} />
-                <Route path="/project" element={<AuthRoute>
-                    <ProjectPage />
-                </AuthRoute>} />
-                <Route path="/chat" element={<AuthRoute>
-                    <ChatPage />
-                </AuthRoute>} />
-                <Route path="/profile" element={<AuthRoute>
-                    <ProfilePage />
-                </AuthRoute>} />
+          {/* main pages */}
+          <Route path="/home" element={
+             <AuthRoute>
+                <HomePage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/project"
+            element={
+              <AuthRoute>
+                <ProjectPage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              <AuthRoute>
+                <ChatPage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <AuthRoute>
+                <ProfilePage />
+              </AuthRoute>
+            }
+          />
 
+          {/* Chat section nae routes */}
+          <Route
+            path="/chat/find"
+            element={
+              <AuthRoute>
+                <SearchMember />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/chat/chatting"
+            element={
+              <AuthRoute>
+                <Chatting />
+              </AuthRoute>
+            }
+          />
 
-                {/* Chat section nae routes */}
-                <Route path="/chat/find" element={<AuthRoute>
-                    <SearchMember />
-                </AuthRoute>} />
-                <Route path="/chat/chatting" element={<AuthRoute>
-                    <Chatting />
-                </AuthRoute>} />
+          {/* Project section nae routes    */}
+          <Route
+            path="/createProject"
+            element={
+              <AuthRoute>
+                <CreateProject />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/manageProject"
+            element={
+              <AuthRoute>
+                <ManageProject />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/manageProject/:id"
+            element={
+              <AuthRoute>
+                <ProjectView />
+              </AuthRoute>
+            }
+          />
 
+          {/* Task section nae routes */}
+          <Route
+            path="/task"
+            element={
+              <AuthRoute>
+                <TaskPage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/createTask"
+            element={
+              <AuthRoute>
+                <CreateTask />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/manageTask"
+            element={
+              <AuthRoute>
+                <ManageTask />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/manageTask/:id"
+            element={
+              <AuthRoute>
+                <TaskView />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/manageTask/calendar"
+            element={
+              <AuthRoute>
+                <Calendar />
+              </AuthRoute>
+            }
+          />
 
-                {/* Project section nae routes    */}
-                <Route path="/createProject" element={<AuthRoute>
-                    <CreateProject />
-                </AuthRoute>} />
-                <Route path="/manageProject" element={<AuthRoute>
-                    <ManageProject />
-                </AuthRoute>} />
-                <Route path="/manageProject/:id" element={<AuthRoute>
-                    <ProjectView />
-                </AuthRoute>} />
-
-
-                {/* Task section nae routes */}
-                <Route path="/task" element={<AuthRoute>
-                    <TaskPage />
-                </AuthRoute>} />
-                <Route path="/createTask" element={<AuthRoute>
-                    <CreateTask />
-                </AuthRoute>} />
-                <Route path="/manageTask" element={<AuthRoute>
-                    <ManageTask />
-                </AuthRoute>} />
-                <Route path="/manageTask/:id" element={<AuthRoute>
-                    <TaskView />
-                </AuthRoute>} />
-                <Route path="/manageTask/calendar" element={<AuthRoute>
-                    <Calendar />
-                </AuthRoute>} />
-
-
-                {/* File section nae routes    */}
-                <Route path="/file" element={<AuthRoute>
-                    <FilePage />
-                </AuthRoute>} />
-                <Route path="/uploadFile" element={<AuthRoute>
-                    <UploadFile />
-                </AuthRoute>} />
-                <Route path="/manageFile" element={<AuthRoute>
-                    <ManageFile />
-                </AuthRoute>} />
-
-
-            </Routes>
-        </>
-    )
+          {/* File section nae routes    */}
+          <Route
+            path="/file"
+            element={
+              <AuthRoute>
+                <FilePage />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/uploadFile"
+            element={
+              <AuthRoute>
+                <UploadFile />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/manageFile"
+            element={
+              <AuthRoute>
+                <ManageFile />
+              </AuthRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
