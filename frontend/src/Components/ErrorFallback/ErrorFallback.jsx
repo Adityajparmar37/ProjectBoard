c22013fbb4c6c8fb01191ba0a632a21024832791
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-03 13:42:31 +0530

    Minor changes in Error UI

--- frontend/src/Components/ErrorFallback/ErrorFallback.jsx
+++ frontend/src/Components/ErrorFallback/ErrorFallback.jsx
@@ -3,11 +3,15 @@ function ErrorFallback({
   resetErrorBoundary,
 }) {
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>
-        Try again !
+    <div
+      role="alert"
+      className="w-full h-screen flex flex-col justify-center items-center">
+      <p className="text-2xl text-center text-red-400 font-semibold">
+        😥 Something went wrong: {" "}
+        <span className="text-black font-bold">{error.message}</span>
+      </p>
+      <button onClick={resetErrorBoundary} className="bg-gray-100 p-2 font-semibold text-lg mt-5 rounded-md shadow-md hover:shadow-inner text-black hover:text-green-600">
+       Please Try Again !
       </button>
     </div>
   );
