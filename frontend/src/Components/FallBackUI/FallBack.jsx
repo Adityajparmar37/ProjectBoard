d0194810c7b0109320a3e3d35f49c72d41d4cf4f
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-03 12:03:25 +0530

    minor changes

--- frontend/src/Components/FallBackUI/FallBack.jsx
+++ frontend/src/Components/FallBackUI/FallBack.jsx
@@ -4,8 +4,8 @@ export default function FallBack() {
   return (
     <div className="fixed w-full h-full bg-WhiteLoading-rgba z-50 top-0 left-0">
       <div className="flex flex-col justify-center items-center w-full h-[80%]">
-        <h1 className="text-green-400 font-semibold text-2xl text-[1.5rem]">
-          Thank you for Waiting ... 😃
+        <h1 className="text-orange-500 text-[1.5rem]">
+          Loading...
         </h1>
       </div>
     </div>
--- frontend/src/Components/Loading/Loading.jsx
+++ frontend/src/Components/Loading/Loading.jsx
@@ -1,15 +1,17 @@
-import { useLoading } from '../../Hooks/useLoading';
+import { useLoading } from "../../Hooks/useLoading";
 
 export default function Loading() {
-    const { isLoading } = useLoading();
-    if (!isLoading) return;
+  const { isLoading } = useLoading();
+  if (!isLoading) return;
 
-    return (
-        <div className="fixed w-full h-full bg-WhiteLoading-rgba z-50 top-0 left-0">
-            <div className="flex flex-col justify-center items-center w-full h-[80%]">
-                <img src="/loading.svg" alt="Loading!" />
-                <h1 className='text-orange-500 text-[1.5rem]'>Loading...</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className="fixed w-full h-full bg-WhiteLoading-rgba z-50 top-0 left-0">
+      <div className="flex flex-col justify-center items-center w-full h-[80%]">
+        <img src="/loading.svg" alt="Loading!" />
+        <h1 className="text-orange-500 text-[1.5rem]">
+          Loading...
+        </h1>
+      </div>
+    </div>
+  );
 }
