e33b46e18d0dc9aa72eb1bdbe1853c1e415b6b2f
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-03 13:35:58 +0530

    Error boundary added 😅🫡🫡

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,30 @@ import { useLoading } from "./Hooks/useLoading";
 import { useEffect } from "react";
 import setLoadingInterceptor from "./Interceptors/loadingInterceptor";
 import Loading from "./Components/Loading/Loading";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./Components/ErrorFallback/ErrorFallback";
 
 export default function App() {
-
-  const { showLoading, hideLoading } = useLoading();
+  const { showLoading, hideLoading } =
+    useLoading();
 
   useEffect(() => {
-    setLoadingInterceptor({ showLoading, hideLoading });
+    setLoadingInterceptor({
+      showLoading,
+      hideLoading,
+    });
   }, []);
   return (
     <>
-      <Loading />
-      <Header />
-      <AppRoute />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          window.location.reload();
+        }}>
+        <Loading />
+        <Header />
+        <AppRoute />
+      </ErrorBoundary>
     </>
-  )
+  );
 }
--- frontend/src/Pages/ProjectPage/ProjectPage.jsx
+++ frontend/src/Pages/ProjectPage/ProjectPage.jsx
@@ -3,25 +3,35 @@ import { MdOutlineCreateNewFolder } from "react-icons/md";
 import { GrDocumentUpdate } from "react-icons/gr";
 
 export default function ProjectPage() {
-    return (
-        <div className="bg-gray-100 h-screen">
-            <div className="grid grid-cols-3 gap-5 pt-20">
-                <MiniCard
-                    to="/createProject"
-                    bgColor="bg-orange-600"
-                    icon={<MdOutlineCreateNewFolder className="text-[2.5rem] text-orange-600" />}
-                    title="Create Project"
-                    content={["Create a fresh new Project"]}
-                />
+  if (1) {
+    throw new Error("Testing ErrorBoundary");
+  }
+  return (
+    <div className="bg-gray-100 h-screen">
+      <div className="grid grid-cols-3 gap-5 pt-20">
+        <MiniCard
+          to="/createProject"
+          bgColor="bg-orange-600"
+          icon={
+            <MdOutlineCreateNewFolder className="text-[2.5rem] text-orange-600" />
+          }
+          title="Create Project"
+          content={["Create a fresh new Project"]}
+        />
 
-                <MiniCard
-                    to="/manageProject"
-                    bgColor="bg-orange-600"
-                    icon={<GrDocumentUpdate className="text-[2.5rem] text-orange-600" />}
-                    title="Project Dashboard"
-                    content={["Manage Ongoing Project", "Update existing Project"]}
-                />
-            </div>
-        </div>
-    )
+        <MiniCard
+          to="/manageProject"
+          bgColor="bg-orange-600"
+          icon={
+            <GrDocumentUpdate className="text-[2.5rem] text-orange-600" />
+          }
+          title="Project Dashboard"
+          content={[
+            "Manage Ongoing Project",
+            "Update existing Project",
+          ]}
+        />
+      </div>
+    </div>
+  );
 }
