4c3a781b9eb48aa30aa431c9c4ff31e324665d14
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-08 07:49:14 +0530

    node security concern middlware added 🔒🔒

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const dbconnect = require('./config/database.config');
-const http = require('http');
-const studentRouters = require('./src/routers/student.router.js');
-const projectRouters = require('./src/routers/project.routers.js');
-const chatRouters = require('./src/routers/chat.routers.js');
-const taskRouters = require('./src/routers/task.routers.js');
-const fileRouters = require('./src/routers/file.routers.js');
-const socketConfig = require('./src/socket/socket.js');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const dbconnect = require("./config/database.config");
+const http = require("http");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const studentRouters = require("./src/routers/student.router.js");
+const projectRouters = require("./src/routers/project.routers.js");
+const chatRouters = require("./src/routers/chat.routers.js");
+const taskRouters = require("./src/routers/task.routers.js");
+const fileRouters = require("./src/routers/file.routers.js");
+const socketConfig = require("./src/socket/socket.js");
 
 dotenv.config();
 const app = express();
@@ -17,11 +20,24 @@ app.use(express.json());
 const PORT = process.env.PORT;
 dbconnect();
 
-app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:5173']
-}));
+//security checks
 
+// Use Helmet middleware to secure HTTP headers
+app.use(helmet());
+// Use xss-clean middleware to prevent XSS attacks
+app.use(xss());
+// Use express-mongo-sanitize middleware to prevent MongoDB Injection attacks
+app.use(mongoSanitize());
+
+//cross origin permission
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  })
+);
+
+//API Endpoints
 app.use("/api/student", studentRouters);
 app.use("/api/project", projectRouters);
 app.use("/api/chat", chatRouters);
@@ -34,15 +50,17 @@ socketConfig(server);
 
 // Internal Error Handling
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        message,
-        statusCode,
-    });
+  const statusCode = err.statusCode || 500;
+  const message =
+    err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
 
+//server creation
 server.listen(PORT, () => {
-    console.log(`serving on ${PORT}`);
+  console.log(`serving on ${PORT}`);
 });
